314cf54cc582b983e4441a8e217e840ded656281
Khj3m <109201777+lolvuongong@users.noreply.github.com>
2024-04-26 03:15:09 +0700

    Finish forgot-password and password-reset

--- src/routes/reset-password.js
+++ src/routes/reset-password.js
@@ -1,49 +1,81 @@
 import React, { useState } from "react";
 import API from "../apis/client";
 import { useParams } from "react-router-dom";
+import { TextField, Typography, Container, Button } from "@mui/material";
+import { useNavigate } from "react-router-dom";
 
 const ResetPasswordPage = () => {
+  const navigate = useNavigate();
   const { token } = useParams();
   const [newPassword, setNewPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
-  const [message, setMessage] = useState("");
+  const [error, setError] = useState("");
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
       if (newPassword !== confirmPassword) {
-        setMessage("Passwords do not match");
+        setError("Passwords do not match");
         return;
       }
       await API.post(`/auth/reset-password/${token}`, { newPassword });
-      setMessage("Password reset successfully");
+      navigate("/login");
     } catch (error) {
-      setMessage(error.response.data.message);
+      console.log(error);
     }
   };
 
   return (
-    <div>
-      <h2>Reset Password</h2>
-      <form onSubmit={handleSubmit}>
-        <input
+    <Container
+      maxWidth="md"
+      sx={{
+        mt: 3,
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        minHeight: "50vh",
+      }}
+    >
+      <Typography variant="h4" gutterBottom mb={4}>
+        Reset Password
+      </Typography>
+      <form onSubmit={handleSubmit} style={{ width: "60%" }}>
+        <TextField
+          id="password"
           type="password"
+          label="Enter new password"
+          variant="outlined"
+          fullWidth
+          margin="normal"
           placeholder="Enter new password"
           value={newPassword}
           onChange={(e) => setNewPassword(e.target.value)}
           required
         />
-        <input
+        <TextField
+          id="confirm-password"
           type="password"
+          label="Confirm new password"
+          fullWidth
+          margin="normal"
           placeholder="Confirm new password"
           value={confirmPassword}
           onChange={(e) => setConfirmPassword(e.target.value)}
           required
+          error={error !== ""}
+          helperText={error}
         />
-        <button type="submit">Reset Password</button>
+        <Button
+          type="submit"
+          variant="contained"
+          color="primary"
+          sx={{ mt: 2 }}
+        >
+          Reset Password
+        </Button>
       </form>
-      <p>{message}</p>
-    </div>
+    </Container>
   );
 };
 
