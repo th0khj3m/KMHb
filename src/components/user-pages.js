53f6c52f42f22484df3392d1afacb5935ffa5d78
Khj3m <109201777+lolvuongong@users.noreply.github.com>
2024-05-01 21:53:01 +0700

    Finish sort by and filter by

--- src/components/user-pages.js
+++ src/components/user-pages.js
@@ -49,9 +49,14 @@ export default function UserPage({ fetchedDataDetails, data, type }) {
 
   const filteredAndSortedData =
     data &&
+    ratings &&
     data.slice().sort((a, b) => {
       const movieA = fetchedDataDetails[a.movie_id]?.movieDetails;
       const movieB = fetchedDataDetails[b.movie_id]?.movieDetails;
+      const movieARating =
+        ratings.find((rating) => rating.movie_id === a.movie_id)?.rating || 0;
+      const movieBRating =
+        ratings.find((rating) => rating.movie_id === b.movie_id)?.rating || 0;
       switch (filterBy) {
         case "date_added":
           return sortBy === "asc"
@@ -61,6 +66,14 @@ export default function UserPage({ fetchedDataDetails, data, type }) {
           return sortBy === "asc"
             ? movieA.vote_average - movieB.vote_average
             : movieB.vote_average - movieA.vote_average;
+        case "user_rating":
+          return sortBy === "asc"
+            ? movieARating - movieBRating
+            : movieBRating - movieARating;
+        case "release_date":
+          return sortBy === "asc"
+            ? new Date(movieA.release_date) - new Date(movieB.release_date)
+            : new Date(movieB.release_date) - new Date(movieA.release_date);
         default:
           return true;
       }
