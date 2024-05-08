94f0d001a8798f3df8ad0b3674895eea82071302
Khj3m <109201777+lolvuongong@users.noreply.github.com>
2024-05-08 18:33:39 +0700

    Finish changing to connection string

--- server/db/db.js
+++ server/db/db.js
@@ -3,17 +3,17 @@
 import { Pool } from "postgres-pool";
 import { DB } from "../config.js";
 
-// const pool = new Pool({
-//   connectionString: `postgres://${DB.USER}:${DB.PASSWORD}@${DB.HOST}/${DB.DATABASE}`,
-// });
-
-export const pool = new Pool({
-  user: DB.USER,
-  host: DB.HOST,
-  database: DB.DATABASE,
-  password: DB.PASSWORD,
-  port: DB.PORT,
+const pool = new Pool({
+  connectionString: DB.CONNECTION_STRING,
 });
+
+// export const pool = new Pool({
+//   user: DB.USER,
+//   host: DB.HOST,
+//   database: DB.DATABASE,
+//   password: DB.PASSWORD,
+//   port: DB.PORT,
+// });
 
 export default {
   query: (text, params) => pool.query(text, params),
